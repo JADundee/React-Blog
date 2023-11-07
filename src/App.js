43ffa83504090b93e9a8f29aa7a880f7057d054f
JADundee <Jadundee@icloud.com>
2023-11-07 04:33:06 -0700

    Custom hooks and icons added

--- src/App.js
+++ src/App.js
@@ -17,6 +17,8 @@ import {
 } from 'react-router-dom';
 import { useState, useEffect } from "react"
 import api from './api/posts'
+import useWindowSize from "./hooks/useWindowSize"
+import useAxiosFetch from "./hooks/useAxiosFetch"
 
 
 
@@ -28,27 +30,14 @@ function App() {
   const [postBody, setPostBody] = useState('')
   const [editBody, setEditBody] = useState('')
   const [editTitle, setEditTitle] = useState('')
-
   const history = useHistory()
+  const { width } = useWindowSize()
+
+  const { data, fetchError, isLoading } = useAxiosFetch(`http://localhost:3500/posts`)
 
   useEffect(() => {
-    const fetchPosts = async () => {
-      try{
-        const response = await api.get('/posts')
-        setPosts(response.data)
-      } catch (err) {
-        if (err.message) {
-          console.log(err.response.data)
-          console.log(err.response.status)
-          console.log(err.response.headers)
-        } else {
-          console.log(`Error: ${err.message}`)
-        }
-      }
-    }
-
-    fetchPosts();
-  }, [])
+    setPosts(data)
+  }, [data])
 
   useEffect(() => {
     const filteredResults = posts.filter(post => 
@@ -129,11 +118,15 @@ function App() {
 
   return (
     <div className="App">
-      <Header />
+      <Header title="React Blog" width={width}/>
       <Nav search={search} setSearch={setSearch} />
       <Switch>
         <Route path="/">
-          <Home posts={searchResults} />
+          <Home 
+            posts={searchResults}
+            fetchError={fetchError}
+            isLoading={isLoading}   
+          />
         </Route>
         <Route path="/post">
           <NewPost 
--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import Feed from './Feed'
 
-const Home = ({ posts }) => {
+const Home = ({ posts, fetchError, isLoading }) => {
   return (
     <main className='Home'>
-        {posts.length ? (
-            <Feed posts={posts} />
-        ) : (
-            <p style={{marginTop: "2rem"}}> No Posts to Display</p>
-        )}
+        {isLoading && <p className='statusMsg'>Loading posts...</p>}
+        {!isLoadinf && fetchError && <p className='statusMsg' style={{color: "red"}}>{fetchError}</p>}
+          {!isLoading && !fetchError && (posts.length ? 
+          <Feed posts={posts}/> : 
+          <p className='statusMg'>No posts to display.</p>)}
     </main>
   )
 }
